e7a4fd3357b117f3e1b7e4107bff323cfd626173
Ripunjayyadav <82287367+Ripunjayyadav@users.noreply.github.com>
2022-11-25 01:12:34 +0530

    FRIENDSHUB

---
+++ assets/pages/post.php
@@ -0,0 +1,27 @@
+
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add New Post</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="img/post.jpg" class="w-100 rounded border">
+                <form>
+                    <div class="my-3">
+
+                        <input class="form-control" type="file" id="formFile">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Say Something</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary">Post</button>
+            </div>
+        </div>
+    </div>
+
